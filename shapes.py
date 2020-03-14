c3bb6a220d93465b5a578f89d7264f7428442363
Maleda Tessema <mtessema@zipcodes-MBP-3.lan>
2020-03-14 14:53:39 -0400

    Pt 11/ Ex 1

---
+++ shapes.py
@@ -0,0 +1,14 @@
+class Rectangle:
+	pass
+shape_rectangle = Rectangle()
+	shape_rectangle.length = ''
+	shape_rectangle.width = ''
+def __ init__(self, area):
+	self.area = area
+
+def __init__(self, perimeter):
+	self.perimeter = perimeter
+
+class Square:
+	pass
+
