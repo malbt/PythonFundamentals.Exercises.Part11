90623431bce93f956b883e7d124f8f8360277f9e
Maleda Tessema <mtessema@zipcodes-MBP-3.lan>
2020-03-14 20:11:39 -0400

    Pt 11 / Ex 1

--- shapes.py
+++ shapes.py
@@ -17,17 +17,4 @@ class Rectangle:
         print(2 * (self.length + self.width))
 
 
-class Square:
 
-    def __init__(self):
-        self.Rectangle = rectangle()
-
-    import shapes
-    rect = shapes.Rectangle(2, 4)
-    rect.area()
-    # line 27 AttributeError: partially initialized module 'shapes' has no attribute 'Square' (most likely due to a
-    # circular import)
-    square = shapes.Square(8)
-    square.area()
-
-    square.perimeter()
